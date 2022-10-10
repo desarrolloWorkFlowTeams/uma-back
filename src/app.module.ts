ccffe2d75cc8060ee720e1adbf51b75f7c512e44
Luillin Escobar <cataplum.17@hotmail.com>
2022-10-10 09:23:02 -0500

    initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration/configuration';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '60mb' }));
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Documentation course API Krensi')
+    .setDescription('Documentation course API Krensi')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-documentation', app, document);
+  await app
+    .listen(process.env.API_PORT)
+    .then(() =>
+      console.log(
+        `API Run at https://${process.env.API_HOST}:${process.env.API_PORT}`,
+      ),
+    );
 }
-bootstrap();
+bootstrap().then();
